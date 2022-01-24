797ee917e65f149ba767aeb4f169277c6a77b8ec
Cactooz <54435874+Cactooz@users.noreply.github.com>
2022-01-24 14:21:14 +0100

    Each 3rd character

--- Lab 1/analyze.asm
+++ Lab 1/analyze.asm
@@ -4,19 +4,18 @@
 
 	.text
 main:
-	li	$s0,0x30
+	li $s0,0x30
 loop:
-	move	$a0,$s0		# copy from s0 to a0
+	move $a0,$s0		# copy from s0 to a0
 	
-	li	$v0,11		# syscall with v0 = 11 will print out
+	li $v0,11			# syscall with v0 = 11 will print out
 	syscall			# one byte from a0 to the Run I/O window
 
-	addi	$s0,$s0,1	# what happens if the constant is changed?
+	addi	$s0,$s0,3		#3, makes the code jump 3 steps
 	
-	li	$t0,0x5b
-	bne	$s0,$t0,loop
-	nop			# delay slot filler (just in case)
+	li $t0,0x5D		#stop when we reach 3 places after Z
+	bne $s0,$t0,loop
+	nop				# delay slot filler (just in case)
 
-stop:	j	stop		# loop forever here
-	nop			# delay slot filler (just in case)
-
+stop:	j stop			# loop forever here
+	nop				# delay slot filler (just in case)
