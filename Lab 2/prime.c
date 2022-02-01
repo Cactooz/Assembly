d1380a44060d5d1a6c0d4fad5ea0a111985f4d4b
Cactooz <54435874+Cactooz@users.noreply.github.com>
2022-02-01 10:41:02 +0100

    Function to find prime numbers

--- Lab 2/prime.c
+++ Lab 2/prime.c
@@ -7,7 +7,16 @@
 #include <stdio.h>
 
 int is_prime(int n){
-	return 0;
+	//Return instantly if n = 1 or 2
+	if(n == 1 || n == 2) return 1;
+	
+	//If n can be divided return 0
+	for(int i = 2; i < n/2; i++) {
+		if(n%i == 0) return 0;
+	}
+
+	//Else return 1
+	return 1;
 }
 
 int main(void){
