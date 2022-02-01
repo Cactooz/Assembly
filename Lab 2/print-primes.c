47f7ceed80cc9be09aafd310247d186415041fb2
Cactooz <54435874+Cactooz@users.noreply.github.com>
2022-02-01 11:24:28 +0100

    print_primes print all numbers up to the defined number

--- Lab 2/print-primes.c
+++ Lab 2/print-primes.c
@@ -9,25 +9,6 @@
 
 #define COLUMNS 6
 
-void print_primes(int n){
-	// Should print out all prime numbers less than 'n'
-	// with the following formatting. Note that
-	// the number of columns is stated in the define
-	// COLUMNS
-
-	printf("%10d ", 2);
-	printf("%10d ", 3);
-	printf("%10d ", 5);
-	printf("%10d ", 7);
-	printf("%10d ", 11);
-	printf("%10d ", 13);
-	printf("\n");
-	printf("%10d ", 17);
-	printf("%10d ", 19);
-
-	printf("\n");
-}
-
 int currentColumn = 0;
 void print_numbers(int n){
 	//Print the number
@@ -52,6 +33,17 @@ int is_prime(int n){
 
 	//Else return 1
 	return 1;
+}
+
+void print_primes(int n){
+	// Should print out all prime numbers less than 'n'
+	// with the following formatting. Note that
+	// the number of columns is stated in the define
+	// COLUMNS
+	for(int i = 2; i <= n; i++){
+		if(is_prime(i))
+			print_numbers(i);
+	}
 }
 
 // 'argc' contains the number of program arguments, and
