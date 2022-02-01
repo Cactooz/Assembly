f07dcd4e72a391edca118356dc4666962f19b1a1
Cactooz <54435874+Cactooz@users.noreply.github.com>
2022-02-01 11:23:06 +0100

    Added missing = in is_prime for

--- Lab 2/print-primes.c
+++ Lab 2/print-primes.c
@@ -46,7 +46,7 @@ int is_prime(int n){
 	if(n == 0 || n == 1) return 0;
 	
 	//If n can be divided return 0
-	for(int i = 2; i < n/2; i++) {
+	for(int i = 2; i <= n/2; i++) {
 		if(n%i == 0) return 0;
 	}
 
