85b5052e8f00995bbe8ac3dba11e5950510aabab
Cactooz <54435874+Cactooz@users.noreply.github.com>
2022-02-07 22:35:52 +0100

    Add input = 2, return 2 check

--- Lab 2/sieves.c
+++ Lab 2/sieves.c
@@ -17,6 +17,12 @@ void print_numbers(int n){
 }
 
 void print_sieves(int n){
+	//Just print 2, if n = 2
+	if(n == 2) {
+		printf("2");
+		return;
+	}
+
 	int numbers[n-2];
 
 	//Fill the array with the numbers from 2
