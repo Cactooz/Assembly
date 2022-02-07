203039c32e30297ebabb0db3939c9e83aee6ee2b
Cactooz <54435874+Cactooz@users.noreply.github.com>
2022-02-07 11:51:55 +0100

    Remove print_primes, use print_sieves

--- Lab 2/sieves.c
+++ Lab 2/sieves.c
@@ -24,22 +24,13 @@ void print_sieves(int n){
 		numbers[i] = i;
 	}
 }
-void print_primes(int n){
-	// Should print out all prime numbers less than 'n'
-	// with the following formatting. Note that
-	// the number of columns is stated in the define
-	// COLUMNS
-	for(int i = 2; i <= n; i++){
-		print_sieves(i);
-	}
-}
 
 // 'argc' contains the number of program arguments, and
 // 'argv' is an array of char pointers, where each
 // char pointer points to a null-terminated string.
 int main(int argc, char *argv[]){
 	if(argc == 2)
-		print_primes(atoi(argv[1]));
+		print_sieves(atoi(argv[1]));
 	else
 		printf("Please state an interger number.\n");
 	return 0;
